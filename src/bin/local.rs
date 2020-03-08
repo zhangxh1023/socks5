4a77c7cbe250b98f1770cdae06e008fc4324fc6f
zhangxh1023 <zhangxh1023@qq.com>
2020-03-08 13:09:52 +0800

    local socks5 listening service

--- src/bin/local.rs
+++ src/bin/local.rs
@@ -1,3 +1,32 @@
+use std::net::{ TcpListener, TcpStream, Shutdown };
+use std::thread;
+use std::io::{ copy };
+
 fn main() {
-  unimplemented!();
+  let listener = TcpListener::bind("127.0.0.1:1186").unwrap();
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
+    thread::spawn(move || {
+      socks5_handle(stream);
+    });
+  }
+}
+
+fn socks5_handle(stream: TcpStream) {
+  let socks5_service = TcpStream::connect("127.0.0.1:8080").unwrap();
+  let mut client_reader = stream.try_clone().unwrap();
+  let mut socket_writer = socks5_service.try_clone().unwrap();
+
+  thread::spawn(move || {
+    copy(&mut client_reader, &mut socket_writer).unwrap();
+    client_reader.shutdown(Shutdown::Read).unwrap();
+    socket_writer.shutdown(Shutdown::Write).unwrap();
+  });
+
+  let mut client_writer = stream.try_clone().unwrap();
+  let mut socket_reader = socks5_service.try_clone().unwrap();
+  
+  copy(&mut socket_reader, &mut client_writer).unwrap();
+  socket_reader.shutdown(Shutdown::Read).unwrap();
+  client_writer.shutdown(Shutdown::Write).unwrap();
 }
