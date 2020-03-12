3746ac8c70c784fb8d55be077bf048469e0b34f9
zhangxh1023 <zhangxh1023@qq.com>
2020-03-12 21:52:17 +0800

    todo: async tcpstream copy

--- src/bin/_server.rs
+++ src/bin/_server.rs
@@ -1,13 +1,15 @@
-use async_std::net::{ TcpListener, TcpStream, ToSocketAddrs, SocketAddr, IpAddr, Ipv4Addr };
+// use async_std::io::copy;
+use async_std::net::{
+  IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener, TcpStream, ToSocketAddrs,
+};
 use async_std::prelude::*;
 use async_std::task;
 use std::str;
-use async_std::io::copy;
 
 const SOCKS5_VERSION: u8 = 0x05;
 const SOCKS5_AUTH_METHOD_NONE: u8 = 0x00;
 const SOCKS5_CMD_TCP_CONNECT: u8 = 0x01;
-const _SOCKS5_RSV: u8 = 0x00;
+const SOCKS5_RSV: u8 = 0x00;
 const SOCKS5_ADDR_TYPE_IPV4: u8 = 0x01;
 const SOCKS5_ADDR_TYPE_DOMAIN: u8 = 0x03;
 const SOCKS5_ADDR_TYPE_IPV6: u8 = 0x04;
@@ -21,6 +23,7 @@ fn main() -> Result<()> {
 
 fn run() -> Result<()> {
   let fut = accept_loop("127.0.0.1:8080");
+  println!("listening 127.0.0.1:8080");
   task::block_on(fut)
 }
 
@@ -32,7 +35,7 @@ async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     println!("Accept from {}", stream.peer_addr()?);
     let _handle = task::spawn(connection_loop(stream));
   }
-  unimplemented!()
+  Ok(())
 }
 
 async fn connection_loop(mut stream: TcpStream) -> Result<()> {
@@ -92,11 +95,11 @@ async fn connection_loop(mut stream: TcpStream) -> Result<()> {
     Domain(Vec<u8>),
     Unknown,
   }
-  let dest_addr_type = match addr_type[0] {
+  let (dest_addr_type, reply_atyp) = match addr_type[0] {
     SOCKS5_ADDR_TYPE_IPV4 => {
       let mut ipv4_addr = [0u8; 4];
       stream.read_exact(&mut ipv4_addr).await?;
-      DestAddrType::Ipv4(ipv4_addr)
+      (DestAddrType::Ipv4(ipv4_addr), SOCKS5_ADDR_TYPE_IPV4)
     }
     SOCKS5_ADDR_TYPE_DOMAIN => {
       let mut domain_size = [0u8; 1];
@@ -104,45 +107,103 @@ async fn connection_loop(mut stream: TcpStream) -> Result<()> {
       let domain_size = domain_size[0] as usize;
       let mut domain_addr = vec![0u8; domain_size];
       stream.read_exact(&mut domain_addr).await?;
-      DestAddrType::Domain(domain_addr)
+      (DestAddrType::Domain(domain_addr), SOCKS5_ADDR_TYPE_DOMAIN)
     }
-    SOCKS5_ADDR_TYPE_IPV6 => DestAddrType::Unknown,
-    _ => DestAddrType::Unknown,
+    SOCKS5_ADDR_TYPE_IPV6 => (DestAddrType::Unknown, 0),
+    _ => (DestAddrType::Unknown, 0),
   };
   let mut dest_addr_port = [0u8; 2];
   stream.read_exact(&mut dest_addr_port).await?;
-  let dest_addr_port = dest_addr_port[0] as u16 * 256 + dest_addr_port[1] as u16;
+  // +----+-----+-------+------+----------+----------+
+  // |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+  // +----+-----+-------+------+----------+----------+
+  // | 1  |  1  | X'00' |  1   | Variable |    2     |
+  // +----+-----+-------+------+----------+----------+
   //
   // @todo reply client
   //
+  let reply_vec = vec![
+    SOCKS5_VERSION,
+    SOCKS5_REPLY_SUCCEEDED,
+    SOCKS5_RSV,
+    reply_atyp,
+  ];
+  match &dest_addr_type {
+    DestAddrType::Ipv4(ipv4_addr) => {
+      let reply = [reply_vec, ipv4_addr.to_vec(), dest_addr_port.to_vec()].concat();
+      stream.write(&reply).await?;
+    }
+    DestAddrType::Domain(domain_addr) => {
+      let domain_size = domain_addr.len();
+      let domain_size = vec![domain_size as u8];
+      let reply = [
+        reply_vec,
+        domain_size,
+        domain_addr.clone(),
+        dest_addr_port.to_vec(),
+      ]
+      .concat();
+      stream.write(&reply).await?;
+    }
+    _ => (),
+  };
+  let dest_addr_port = dest_addr_port[0] as u16 * 256 + dest_addr_port[1] as u16;
   enum DestStream {
     DestStream(TcpStream),
-    Unknown(String)
+    Unknown(String),
   }
-  let stream = match dest_addr_type {
+  let dest_stream = match dest_addr_type {
     DestAddrType::Ipv4(ipv4_addr) => {
-      let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(ipv4_addr[0], ipv4_addr[1], ipv4_addr[2], ipv4_addr[3])), dest_addr_port);
+      let socket_addr = SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(
+          ipv4_addr[0],
+          ipv4_addr[1],
+          ipv4_addr[2],
+          ipv4_addr[3],
+        )),
+        dest_addr_port,
+      );
       let dest_stream = TcpStream::connect(socket_addr).await?;
       DestStream::DestStream(dest_stream)
-    },
+    }
     DestAddrType::Domain(domain_addr) => {
       let domain = str::from_utf8(&domain_addr)?;
-      let dest_stream = TcpStream::connect(format!("{}{}", domain, dest_addr_port)).await?;
+      let addr = format!("{}:{}", domain, dest_addr_port);
+      let dest_stream = TcpStream::connect(addr).await?;
       DestStream::DestStream(dest_stream)
-    },
-    _ => DestStream::Unknown("Unknown destination addr type".to_string())
+    }
+    _ => DestStream::Unknown("Unknown destination addr type".to_string()),
   };
-  match stream {
+  match dest_stream {
     DestStream::Unknown(s) => {
+      stream.shutdown(Shutdown::Both)?;
       Err(s)?
-    },
-    DestStream::DestStream(dest_stream) => {
+    }
+    DestStream::DestStream(mut dest_stream) => {
       //
       // @todo copy stream
       //
-      unimplemented!()
-    } 
-  }
+      let mut buff = vec![0u8; 1024];
+      let size = stream.read(&mut buff).await?;
+      buff.truncate(size);
+      println!("{}", str::from_utf8(&buff)?);
+      dest_stream.write(&buff).await?;
+      let mut buff = vec![0u8; 1024];
+      let mut size = dest_stream.read(&mut buff).await?;
+      buff.truncate(size);
+      println!("{}", str::from_utf8(&buff)?);
+      stream.write(&buff).await?;
+      while size == 1024 {
+        buff = vec![0u8; 1024];
+        size = dest_stream.read(&mut buff).await?;
+        buff.truncate(size);
+        println!("{}", str::from_utf8(&buff)?);
+        stream.write(&buff).await?;
+      }
 
-  unimplemented!()
+      dest_stream.shutdown(Shutdown::Both)?;
+      stream.shutdown(Shutdown::Both)?;
+      Ok(())
+    }
+  }
 }
